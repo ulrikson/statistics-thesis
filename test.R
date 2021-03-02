5e46c601cc5b2ab5b654f7865e043c3a0678ed4b
ulrikson <erik.ulrikson@gmail.com>
2021-03-02 14:49:10 +0100

    new garch model

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 library(forecast)
 library(rugarch)
-library(readxl)
 library(aTSA)
+library(fGarch)
 
 
 # Loading data --------------------------------------------------------------------------------
@@ -12,29 +12,38 @@ logreturns = data$logreturns
 ts.plot(logreturns, xlab = "Day", ylab = "Closing price")
 abline(a = 0, 0, col = "red")
 
-# Splitting into train (90%) and test (10%)
-train_size = round(length(logreturns) * 0.9)
-
+# Splitting into train (80%) and test (20%)
+train_size = round(length(logreturns) * 0.8)
 train = logreturns[1:train_size]
 test = logreturns[(train_size+1):length(logreturns)]
-tail(train)
-head(test)
 
 # ARIMA -----------------------------------------------------------------------------
 
 auto_arima = auto.arima(train, ic = "bic", seasonal = FALSE)
 arimaorder(auto_arima)
 arima = arima(train, c(1,0,1)) #  inserting into model (needed for arch.test)
-arima_forecast = predict(arima, n.ahead=5)
+predict(arima, n.ahead=5)
 
-rmse_arima = sqrt(mean((test[1:5] - arima_forecast$pred)^2))
-rmse_arima
-
-plot(arima$residuals, type="p", cex=0.5)
+plot(arima$residuals, type="p", cex=0.5) # Heteroskedasticitet?
 arch.test(arima, output = TRUE) #  H0 förkastas -> heteroskedasticitet?
 
 
-# GARCH --------------------------------------------------------------------------------
+# GARCH with fGarch (one way) ---------------------------------------------------------------------------
+
+fGarch = garchFit(
+  formula = ~ arma(1, 1) + garch(1, 1),
+  data = train,
+  trace = FALSE
+)
+summary(fGarch)
+fgarch_pred = predict(fGarch, n.ahead = 5)
+
+rmse_fGarch = sqrt(mean((test[1:5] - fgarch_pred$meanForecast) ^ 2))
+rmse_fGarch
+
+
+
+# GARCH (another way) --------------------------------------------------------------------------------
 
 #* ARCH(1,1) from auto_arima (1,1)
 arch = ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1))) #  simplest GARCH, a lot available
@@ -45,6 +54,7 @@ plot(arch_fit)
 
 # Forecasting
 forecast = ugarchforecast(arch_fit, n.ahead = 5);
+forecast
 rmse_arch = sqrt(mean((test[1:5] - forecast@forecast$seriesFor)^2))
 rmse_arch
 
@@ -57,7 +67,7 @@ rmse_arch
 sigma(forecast);
 fitted(forecast)
 
-ts.plot(test[1:50])
+ts.plot(test[1:5])
 lines(fitted(forecast), col="red")
 
 
@@ -69,7 +79,7 @@ archInfoCriteria <- function(p, q) {
 }
 
 archInfoCriteria(1,1)
-# very small differences to these other models (< 0.01%)
+# very small differences from (1,1) to the other models (< 0.01%)
 archInfoCriteria(2,1)
 archInfoCriteria(1,2)
 archInfoCriteria(2,2)
