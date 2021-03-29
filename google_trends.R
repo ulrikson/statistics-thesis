b5d783d5f934f072e6753d1f4da3b3cc70992cc1
ulrikson <erik.ulrikson@gmail.com>
2021-03-29 11:44:42 +0200

    updates

--- google_trends.R
+++ google_trends.R
@@ -5,15 +5,21 @@ ml = read.csv('data/ml.csv')
 ml_ts = ts(ml$index, start=c(2010,1), frequency = 12)
 
 
-ts.plot(statistics_ts, ylim=c(0,100), ylab="Sökindex", xlab="Tid", main="Söktrender")
-lines(ml_ts, col="red")
+ts.plot(
+  statistics_ts,
+  ylim = c(0, 100),
+  ylab = "Sökindex",
+  xlab = "Tid",
+  main = "Söktrender 2010-2020"
+)
+lines(ml_ts, col = "red")
 legend(
   x = 'top',
   inset = c(-1, 0),
   legend = c("'statistics'", "'machine learning'"),
-  col = c(1,2),
+  col = c(1, 2),
   lty = c(1, 1),
-  cex=0.75
+  cex = 0.75
 )
 
 
--- test.R
+++ test.R
@@ -12,8 +12,8 @@ logreturns = data$logreturns
 ts.plot(logreturns, xlab = "Day", ylab = "Closing price")
 abline(a = 0, 0, col = "red")
 
-# Splitting into train (80%) and test (20%)
-train_size = round(length(logreturns) * 0.8)
+# Splitting into train and test
+train_size = round(length(logreturns) - 7)
 train = logreturns[1:train_size]
 test = logreturns[(train_size+1):length(logreturns)]
 
@@ -21,39 +21,60 @@ test = logreturns[(train_size+1):length(logreturns)]
 
 auto_arima = auto.arima(train, ic = "bic", seasonal = FALSE)
 arimaorder(auto_arima)
-arima = arima(train, c(1,0,1)) #  inserting into model (needed for arch.test)
-predict(arima, n.ahead=5)
+arima = arima(train, c(1,1,2)) #  inserting into model (needed for arch.test)
+#predict(arima, n.ahead=5)
 
 plot(arima$residuals, type="p", cex=0.5) # Heteroskedasticitet?
 arch.test(arima, output = TRUE) #  H0 förkastas -> heteroskedasticitet?
 
-
 # ARMA-GARCH ---------------------------------------------------------------------------
 
 armaGarch = garchFit(
-  formula = ~ arma(1, 1) + garch(1, 1),
+  formula = ~ arma(1, 2) + garch(1, 1),
   data = train,
   trace = FALSE
 )
 summary(armaGarch)
 armaGarch@fit$ics
-armaGarch_pred = predict(armaGarch, n.ahead = 5)
-armaGarch_pred
 
-rmse_armaGarch = sqrt(mean((test[1:5] - armaGarch_pred$meanForecast) ^ 2))
-rmse_armaGarch
+# 1 day ahead
+armaGarch_1 = predict(armaGarch, n.ahead = 1)
+rmse_1 = sqrt(mean((test[1:1] - armaGarch_1$meanForecast) ^ 2))
+mape_1 = mean(abs((test[1:1] - armaGarch_1$meanForecast) / test[1:1])) * 100
+paste(rmse_1, mape_1)
 
-# Pure ARCH
+# 3 days ahead
+armaGarch_3 = predict(armaGarch, n.ahead = 2)
+rmse_3 = sqrt(mean((test[1:3] - armaGarch_1$meanForecast) ^ 2))
+mape_3 = mean(abs((test[1:3] - armaGarch_1$meanForecast) / test[1:3])) * 100
+paste(rmse_3, mape_3)
+
+# 5 days ahead
+armaGarch_5 = predict(armaGarch, n.ahead = 5)
+rmse_5 = sqrt(mean((test[1:5] - armaGarch_5$meanForecast) ^ 2))
+mape_5 = mean(abs((test[1:5] - armaGarch_5$meanForecast) / test[1:5])) * 100
+paste(rmse_5, mape_5)
+
+ts.plot(logreturns[4427:4562])
+lines(ts(test[1:5], start=132), col="blue")
+lines(ts(armaGarch_5$meanForecast, start=132), col="red")
+
+
+
+# Pure ARCH (just for fun) --------------------------------------------------------------------
+
+
 garch = garchFit(formula = ~ garch(1, 1),
                  data = train,
                  trace = FALSE)
 summary(garch)
-garch@fit$ics
-garch_pred = predict(garch, n.ahead = 5)
-garch_pred
 
-rmse_garch = sqrt(mean((test[1:5] - garch_pred$meanForecast) ^ 2))
-rmse_garch
+# 5 days ahead
+garch_pred = predict(garch, n.ahead = 5)
+garch_rmse_5 = sqrt(mean((test[1:5] - garch_pred$meanForecast) ^ 2))
+garch_mape_5 = mean(abs((test[1:5] - garch_pred$meanForecast) / test[1:5])) * 100
+paste(garch_rmse_5, garch_mape_5)
+
 
 
 
